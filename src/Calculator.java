1c240dc83d009bcb855187ce8e31bc5ba1eba8ec
moostofa <mmdrahinz96@gmail.com>
2021-10-09 11:44:24 +1100

    Updated options - let user choose polynomial degrees

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,31 +7,46 @@ public class Calculator {
     final static double TOL = 0.05;
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        double[] coefficients = new double[3];
         
-        //testing using quadratic equation for now
-        System.out.println("Polynomial form: a*x^2 + b*x + c");
+        //Polynomial function options
+        System.out.println("Polynomial function degrees:");
+        System.out.println("0: f(x) = a*x^0");
+        System.out.println("1: f(x) = a*x^1 + b");
+        System.out.println("2: f(x) = a*x^2 + b*x + c");
+        System.out.println("3: f(x) = a*x^3 + b*x^2 + c*x + d");
+        System.out.println("4: f(x) = a*x^4 + b*x^3 + c*x^2 + dx + e");
+
+        //let user choose an option from the above. Reject if option is incorrect, and re-prompt
+        int degree;
+        do {
+            System.out.print("Which degree polynomial function would you like to create? (chose from 0-4)");
+            degree = input.nextInt();
+        } while (degree < 0 || degree > 4);
+
+        double[] coefficients = new double[degree + 1];
         System.out.println("Enter values for:");
-        for (int i = 0; i < 3; i++) {
-            System.out.print((char)(i + 97) + ": ");                            //this will display a: , b: , and c:
+        for (int i = 0; i < coefficients.length; i++) {
+            System.out.print((char)(i + 97) + ": ");                            //utilising ASCII: this will display a: , b: , c: , and so on
             coefficients[coefficients.length - i - 1] = input.nextDouble();       //stored from end of array: reason below           
         }
 
         //parameters are taken as an array of doubles, in order of increasing degree. i.e., [x^0, x^1, x^2 ... x^n-1]
         PolynomialFunction function = new PolynomialFunction(coefficients);
-        
+        System.out.println("Your polynomial expression: " + function.toString());   //prints the polynomial expression as a string
+
+        //let user choose upper and lower bounds of function
         System.out.println("Set 2 interval points: upper and lower bounds. f(lower) and f(upper) must be of opposite signs.");
         System.out.print("Upper bound: ");
         double upperBound = input.nextDouble();
         System.out.print("Lower bound: ");
         double lowerBound = input.nextDouble();
 
+        //check condition of f(a) and f(b) being of different signs
         if (function.value(upperBound) * function.value(lowerBound) >= 0) {
             System.out.println("Incorrect upper and lower bounds");
             System.exit(0);
         }
         
-        System.out.println("Your polynomial expression: " + function.toString());   //prints the polynomial expression as a string
         System.out.println("Upper bound = " + function.value(upperBound));          //function.value(double a) returns f(a)
         System.out.println("Lower bound: " + function.value(lowerBound));
 
