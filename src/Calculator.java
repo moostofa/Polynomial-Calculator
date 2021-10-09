1997dd0ca2d6b09cdeb943979f22507a651b5f79
moostofa <mmdrahinz96@gmail.com>
2021-10-09 11:27:57 +1100

    Bisection method - working 👌🏼

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,51 +3,56 @@ import java.util.Scanner;
 import org.apache.commons.math3.analysis.polynomials.*;
 
 public class Calculator {
+    final static int MAX = 100;        //max number of iterations
+    final static double TOL = 0.05;
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         double[] coefficients = new double[3];
         
+        //testing using quadratic equation for now
         System.out.println("Polynomial form: a*x^2 + b*x + c");
         System.out.println("Enter values for:");
         for (int i = 0; i < 3; i++) {
-            System.out.print((char)(i + 97) + ": ");
-            coefficients[coefficients.length - i - 1] = input.nextDouble();
+            System.out.print((char)(i + 97) + ": ");                            //this will display a: , b: , and c:
+            coefficients[coefficients.length - i - 1] = input.nextDouble();       //stored from end of array: reason below           
         }
-        for (double i : coefficients)
-            System.out.println(i);
 
+        //parameters are taken as an array of doubles, in order of increasing degree. i.e., [x^0, x^1, x^2 ... x^n-1]
         PolynomialFunction function = new PolynomialFunction(coefficients);
-        System.out.println("Your polynomial expression: " + function.toString());
-
-        System.out.println("Set 2 interval points: upper and lower bounds. They must be of different signs and non-zero.");
+        
+        System.out.println("Set 2 interval points: upper and lower bounds. f(lower) and f(upper) must be of opposite signs.");
         System.out.print("Upper bound: ");
-        double a = input.nextDouble();
+        double upperBound = input.nextDouble();
         System.out.print("Lower bound: ");
-        double b = input.nextDouble();
+        double lowerBound = input.nextDouble();
 
-        if (a * b >= 0) {
-            System.out.println("Invalid upper and lower bounds");
+        if (function.value(upperBound) * function.value(lowerBound) >= 0) {
+            System.out.println("Incorrect upper and lower bounds");
             System.exit(0);
         }
         
-        double tolerance = 0.005;
-        bisection(function, a, b, tolerance);
+        System.out.println("Your polynomial expression: " + function.toString());   //prints the polynomial expression as a string
+        System.out.println("Upper bound = " + function.value(upperBound));          //function.value(double a) returns f(a)
+        System.out.println("Lower bound: " + function.value(lowerBound));
+
+        bisection(function, upperBound, lowerBound);
         input.close();
     }
 
-    public static void bisection(PolynomialFunction function, double a, double b, double tolerance) {
+    public static void bisection(PolynomialFunction function, double a, double b) {
         double c;
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < MAX; i++) {
             c = (a + b)/2;
-            if (function.value(c) == 0 || (b - a)/2 < tolerance) {
-                System.out.println(c + " is a root of the function");
+            if (function.value(c) == 0 || Math.abs((b - a)/2) < TOL) {
+                System.out.println(c + " is a root");
                 return;
             }
-            if ((a < 0 && c < 0) || (a > 0 && c > 0))
+            if (function.value(a) * function.value(c) >= 0)
                 a = c;
             else
                 b = c;
         }
-        System.out.println("Method failed");
+        System.out.println("Method failed.");
+        return;
     }
 }
